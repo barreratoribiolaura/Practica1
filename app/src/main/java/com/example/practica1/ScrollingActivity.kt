608b21fb96b5af3c7b612385dc59cf82ef36a931
laurabarreratoribio <laurabarreratoribio@gmail.com>
2022-10-05 18:17:25 +0200

    commit 3

--- app/src/main/java/com/example/practica1/ScrollingActivity.kt
+++ app/src/main/java/com/example/practica1/ScrollingActivity.kt
@@ -1,6 +1,6 @@
 package com.example.practica1
 
-import android.content.Intent
+import  android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import com.google.android.material.appbar.CollapsingToolbarLayout
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
@@ -33,41 +34,26 @@ class ScrollingActivity : AppCompatActivity() {
         loadImage("https://nexus.leagueoflegends.com/wp-content/uploads/2018/04/LOL_CMS_198_Article_01_ifsox5gchrv42f65its8.jpg",binding.content.img3)
         loadImage("https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/9/9e/Skin_Splash_Classic_Kennen.jpg/revision/latest/scale-to-width-down/350?cb=20191212182720",binding.content.img4)
 
+        onClick("https://www.leagueoflegends.com/es-es/champions/diana/",binding.content.btnEnlace)
+        onClick("https://www.leagueoflegends.com/es-es/champions/pantheon/",binding.content.btnEnlace2)
+        onClick("https://www.leagueoflegends.com/es-es/champions/kai-sa/",binding.content.btnEnlace3)
+        onClick("https://www.leagueoflegends.com/es-es/champions/kennen/",binding.content.btnEnlace4)
 
-        binding.content.btnEnlace.setOnClickListener{
-            val url1: String ="https://www.leagueoflegends.com/es-es/champions/diana/"
-            openURL.data = Uri.parse(url1)
-            startActivity(openURL)
-        }
 
-        binding.content.btnEnlace2.setOnClickListener{
-            val url1: String ="https://www.leagueoflegends.com/es-es/champions/pantheon/"
-            openURL.data = Uri.parse(url1)
-            startActivity(openURL)
-        }
 
-        binding.content.btnEnlace3.setOnClickListener{
-            val url1: String ="https://www.leagueoflegends.com/es-es/champions/kai-sa/"
-            openURL.data = Uri.parse(url1)
-            startActivity(openURL)
-        }
-
-        binding.content.btnEnlace4.setOnClickListener{
-            val url1: String ="https://www.leagueoflegends.com/es-es/champions/kennen/"
-            openURL.data = Uri.parse(url1)
-            startActivity(openURL)
-        }
     }
 
+    //Enlaces
+    private fun onClick(url:String,btn: Button){
+        btn.setOnClickListener{
+            openURL.data= Uri.parse(url)
+            startActivity(openURL)
+        }
 
-
-
-
-
-
+    }
     //Glide
     private fun loadImage(
-        url:String = "https://esports.as.com/2022/01/14/league-of-legends/Diana_1537956204_895415_1440x600.jpg",
+        url:String,
         iv: ImageView){
         Glide.with(this)
             .load(url)
